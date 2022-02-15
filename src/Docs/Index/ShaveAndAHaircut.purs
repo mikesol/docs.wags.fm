acc969ff62b9ab94242402368814279f2d17312e
Mike Solomon <mike@meeshkan.com>
2022-02-15 10:29:01 +0200

    Different intro

--- src/Docs/Index/ShaveAndAHaircut.purs
+++ src/Docs/Index/ShaveAndAHaircut.purs
@@ -6,28 +6,37 @@ import Type.Proxy (Proxy(..))
 import WAGS.Lib.Learn (Player, player)
 import WAGS.Lib.Tidal (tdl)
 import WAGS.Lib.Tidal.Synth (synth)
-import WAGS.Lib.Tidal.Tidal (changeVolume, make, parse, s)
+import WAGS.Lib.Tidal.Tidal (b, changeVolume, i, make, parse, s, x)
 import WAGS.Lib.Tidal.Types (AFuture)
+
+p = parse
+twobits o0 o1 o2 o3 o4 o5 = o0
+  ( o1 (p "c5")
+      [ o2 (p "g4") [ p "g4" ]
+      , p "aflat4"
+      , p "g4"
+      ]
+  )
+  [ o3 (p "~")
+      [ o4 (p "d4") [ p "f4", p "g4", p "b4" ]
+      , o5 (p "c4") [ p "e4", p "g4", p "c5" ]
+      , (p "~")
+      ]
+  ]
 
 wag :: AFuture
 wag =
-  make 6.0
+  make 4.0
     { earth: s $ (map <<< map) (synth {})
-        $ map
-            ( changeVolume
-                ( _.normalizedLittleCycleTime
-                    >>> (_ < 0.33)
-                    >>> if _ then 1.0 else 0.5
-                )
-            )
-        $ parse
-            ( Proxy :: _
-                """
-        [c5 g4*2 aflat4 g4]
-        [~ [d4,f4,g4,b4] [c4,e4,g4,c5] ~] ~"""
-            )
+        $ map (changeVolume (const 0.4))
+        $ b (twobits i i i i x x)
+            [ twobits i x i x i x
+            , twobits i x i x i i
+            , twobits x i x i x i
+            ]
+
     , wind: s $ map (changeVolume (const 0.5))
-        $ parse (Proxy :: _ "~ [~ [hh, bd] [hh, bd] ~] ~")
+        $ parse "~ [~ [hh, bd] [hh, bd] ~]"
     }
 
 main :: Player
